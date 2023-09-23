2a67618a761ae5ea2eeffe9da9a694a7586d4e4f
Maxime Barat <cmaxime1989@hotmail.com>
2023-09-23 10:16:42 +0200

    Initial Commit

---
+++ Livret des internes 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Livret des internes 2
+//
+//  Created by Maxime on 23/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Livret des internes 2/Livret_des_internes_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Livret_des_internes_2App.swift
+//  Livret des internes 2
+//
+//  Created by Maxime on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Livret_des_internes_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
